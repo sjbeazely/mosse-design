2d72fb4addef4e7c2ccf54591f5be420f717f655
sjbeazely <sjbeazely@gmail.com>
2026-01-31 17:16:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("contactForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("contactForm").style.display = "none";
+}
